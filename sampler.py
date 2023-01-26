4d5c00b379d200a395db6c926889d50568fde7aa
= <=>
2023-01-25 21:35:23 -0600

    modify sampler to adjust other dataset

--- sampler.py
+++ sampler.py
@@ -2,32 +2,56 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import numpy as np
 import pandas as pd
+import argparse
 
 from utils.utils import sample_batch_index, binary_sampler
 from tqdm import trange
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-data", type = str, required=True)
+    parser.add_argument("-num_samp", type = str, required=True) # number of samples
+    parser.add_argument("-mr", type = float, required=True) # missing rate
+    parser.add_argument("-size", type = int, required=True) # sample size
+    return parser.parse_args()
+
 if __name__ == '__main__':
     # Load data
-    file_name = 'data/house.csv'
-    house_df = pd.read_csv(file_name)
-    no, dim = house_df.shape
+    args = parse_args()
+    dataset = args.data
 
-    data_x = house_df.values.astype(np.float32)
-    num_samples = 200
+    file_name = 'data/' + dataset + '.csv'
+    data_df = pd.read_csv(file_name)
+    no, dim = data_df.shape
 
-    miss_rate = 0.3
+    data_x = data_df.values.astype(np.float32)
+    num_samples = int(args.num_samp)
+    miss_rate = float(args.mr)
+    sample_size = int(args.size)
+
+    save_path_complete = "./samples/{}/complete_{}_{}".format(dataset,miss_rate, sample_size)
+    if not os.path.exists(save_path_complete):
+            os.makedirs(save_path_complete)
+    save_path_mcar = "./samples/{}/MCAR_{}_{}".format(dataset,miss_rate, sample_size)
+    if not os.path.exists(save_path_mcar):
+            os.makedirs(save_path_mcar)
+
     for i in trange(num_samples):
         # random samples
-        sample_idx = sample_batch_index(no, 10000)
+        sample_idx = sample_batch_index(no, sample_size)
         data_x_i = data_x[sample_idx, :]
         no_i, dim_i = data_x_i.shape
-        np.savetxt("./samples/complete/sample_{}.csv".format(i), data_x_i, delimiter=",")
+        save_path = "./samples/{}/complete_{}_{}/sample_{}.csv".format(dataset,miss_rate, sample_size, i)
+        np.savetxt(save_path, data_x_i, delimiter=",")
 
         # Introduce missing data
         data_m = binary_sampler(1 - miss_rate, no_i, dim_i)
         miss_data_x = data_x_i.copy()
         miss_data_x[data_m == 0] = np.nan
-        np.savetxt("./samples/MCAR/sample_{}.csv".format(i), miss_data_x, delimiter=",")
+        save_path = "./samples/{}/MCAR_{}_{}/sample_{}.csv".format(dataset,miss_rate, sample_size, i)
+        np.savetxt(save_path, miss_data_x, delimiter=",")
+
 
