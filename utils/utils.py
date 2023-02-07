a5d1c3a7ed5e27bd75f883fb6ab916331460b952
jiaxin <sjx@iastate.edu>
2023-02-07 15:54:55 -0600

    fix bug in response sampler

--- utils/utils.py
+++ utils/utils.py
@@ -139,9 +139,9 @@ def binary_sampler(p, rows, cols, seed):
 
 def response_sampler(p, rows, cols, col_index, seed):
   np.random.seed(seed)
-  unif_random_matrix = np.random.uniform(0., 1., size = [rows, len(col_index)])
+  unif_random_matrix = np.random.uniform(0., 1., size = [rows, 1])
   binary_random_matrix = np.ones((rows, cols))
-  binary_random_matrix[:,col_index] = 1 * (unif_random_matrix[:,len(col_index)] < p)
+  binary_random_matrix[:,col_index] = 1 * (unif_random_matrix[:,0] < p)
   return binary_random_matrix.astype('float32')
 
 def uniform_sampler(low, high, rows, cols):
