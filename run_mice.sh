3d07dedc692dfa300a5763482a2d9d628f063b1d
= <=>
2023-02-07 01:14:42 -0600

    add time history for mice command

--- run_mice.sh
+++ run_mice.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+secs=$SECONDS
+bgtime=$(date)
+echo $bgtime
 Rscript mice-cart.R
+edtime=$(date)
+echo $edtime
+hrs=$(( secs/3600 )); mins=$(( (secs-hrs*3600)/60 )); secs=$(( secs-hrs*3600-mins*60 ))
+echo 'Time spent %02d:%02d:%02d\n' $hrs $mins $secs
 echo 'finish'
