5724d61b6b27301b62f07b7ef6c54cce4ecf86b7
= <=>
2023-02-07 01:02:47 -0600

    fix bug for only imputing numeric variables

--- mice-cart.R
+++ mice-cart.R
@@ -5,9 +5,9 @@ library(doParallel)
 library(dplyr)
 
 # register cores
-cores = as.integer(Sys.getenv("SLURM_CPUS_PER_TASK")) - 1
+cores = min(detectCores()-1, 50)
 print(paste("Cores number: ",cores))
-cluster = makeCluster(cores)
+cluster = makeCluster(cores, outfile = "")
 clusterSetRNGStream(cluster, 9956)
 registerDoParallel(cluster)
 
@@ -30,17 +30,20 @@ foreach(i = 0:(sample_size-1), .packages = c("mice"))%dopar%{
   current_seed = 42+i
   set.seed(current_seed)
   
-  complete_file = paste("./samples/",save_name, '/',complete_filefolder, "/sample_",i,".csv", sep = '')
   missing_file = paste("./samples/",save_name, '/',miss_filefolder, "/sample_",i,".csv", sep = '')
-  data_x_i = read.csv(complete_file)
-  data_miss_i = read.csv(missing_file)
+  data_miss_i = read.csv(missing_file, header = FALSE)
   
   print("Change variable types......")
   # change categorical variables into factors
-  cat_index = 1:(dim(data_x_i)[2]-8)
-  num_index = (dim(data_x_i)[2]-7) : dim(data_x_i)[2]
-  data_x_i[,cat_index] = lapply(data_x_i[,cat_index], as.factor)
+  cat_index = 1:(dim(data_miss_i)[2]-8)
   data_miss_i[,cat_index] = lapply(data_miss_i[,cat_index], as.factor)
+  
+  # set NAN level back to NA
+  returnNA = function(x){
+    levels(x)[levels(x) == 'NaN'] <- NA
+    return(x)
+  }
+  data_miss_i[,cat_index] = lapply(data_miss_i[,cat_index], returnNA)
   
   print("Data Imputation......")
   # imputation
@@ -48,11 +51,16 @@ foreach(i = 0:(sample_size-1), .packages = c("mice"))%dopar%{
   
   # output results
   print("Output imputed data......")
-  for (j in 0:(imputed_num - 1)){
+  for (j in 1:imputed_num ){
     data_current_imputed = complete(data_output_i, j)
-    write.csv(data_current_imputed, paste(save_path, '/imputed_',i,'_',j,'.csv',sep = ''))
+    
+    # change categorical variables back to numeric type
+    data_current_imputed[,cat_index] = lapply(data_current_imputed[,cat_index], as.numeric)
+    
+    # remember to ignore row and column names
+    write.table(data_current_imputed, paste(save_path, '/imputed_',i,'_',(j-1),'.csv',sep = ''),
+              sep = ',',col.names = FALSE, row.names = FALSE)
   }
-  
   print(paste("Sample",i,"done."))
 }
 
