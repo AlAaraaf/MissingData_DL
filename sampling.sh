7f2234c2f73ff1f737150f255ee12acac981e111
= <=>
2023-04-03 15:24:57 -0500

    add command for server finetuning

--- sampling.sh
+++ sampling.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-python ./sampler.py -data house -num_samp 10 -mr 0.3 -size 50000 -missc -1
+
+#SBATCH --time=1-0:00:00  # max job runtime
+#SBATCH --cpus-per-task=1  # number of processor cores
+#SBATCH --nodes=1  # number of nodes
+#SBATCH --partition=gpu  # partition(s)
+#SBATCH --gres=gpu:1
+#SBATCH --mem=5G  # max memory
+#SBATCH -J "mdi-gq-t"  # job name
+#SBATCH --mail-user=sjx@iastate.edu  # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#module load python/3.10.8-5qsesua
+#source /work/LAS/zhanruic-lab/jiaxin/vaeac/jiaxin/bin/activate
+
+dataset='income'
+sample_number=1
+mr=0.3
+sample_size=10000
+missing_column=-1
+python ./sampler.py -data $dataset -num_samp ${sample_number} -mr $mr -size ${sample_size} -missc ${missing_column}
 echo 'finish'
