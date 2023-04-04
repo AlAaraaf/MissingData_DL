c237d46f4e3a6f5fbeaec4c6aafe1871961bfd74
= <=>
2023-04-03 22:07:11 -0500

    update tuning list for server task

--- run.sh
+++ run.sh
@@ -15,36 +15,36 @@
 #module load python/3.10.8-5qsesua
 #source /work/LAS/zhanruic-lab/jiaxin/vaeac/jiaxin/bin/activate
 
-degree=1
-model_name="gain_qreg"
-dataset="income"
+model_name="gain"
+dataset="nhanes"
 mr=0.3
-sample_size=20000
+sample_size=5000
 sample_id=$1
-dlr_list=(0.0005 0.0007 0.001 0.002 0.005)
-glr_list=(0.0025 0.0035 0.0005 0.001 0.002)
-d_step=(2 3 4 5)
-g_step=(1 2 3)
+dlr_list=(0.0005 0.001 0.005)
+glr_list=(0.0025 0.0005 0.002)
+d_step=(1 3 5)
+g_step=(1 2)
+batch_size=(64 128 256)
 
-for lr_i in `seq 0 4`
+for bs_i in `seq 0 2`
 do
-    for ds_i in `seq 0 3`
+    for ds_i in `seq 0 2`
     do
-        for gs_i in `seq 0 2`
+        for gs_i in `seq 0 1`
         do
             python ./main.py -id $sample_id -dataset $dataset -model $model_name -mr $mr -size $sample_size \
-            -batch_size 256 \
+            -batch_size ${batch_size[$bs_i]} \
             -alpha 20 \
-            -iterations 100 \
-            -dlr ${dlr_list[$lr_i]} \
-            -glr ${glr_list[$lr_i]} \
+            -iterations 50 \
+            -dlr ${dlr_list[$bs_i]} \
+            -glr ${glr_list[$bs_i]} \
             -d_gradstep ${d_step[$ds_i]} \
             -g_gradstep ${g_step[$gs_i]} \
             -log_name ${model_name}_$dataset/tuning/
 
             python ./calculate_estimands.py -dataset $dataset -model $model_name -num 1 -mr $mr -size $sample_size -completedir ../training_data/samples/${dataset}/complete_${mr}_${sample_size}/ -missingdir ../training_data/samples/${dataset}/MCAR_${mr}_${sample_size}/ -imputedir ../training_data/results/$dataset/MCAR_${mr}_${sample_size}/$model_name/
             python ./evaluate_estimands.py -dataset $dataset -model $model_name
-            python ./show_tables.py -dataset $dataset -output ../metrics/$dataset/${model_name}_${mr}_${sample_size}_${sample_id}_${dlr_list[$lr_i]}_${glr_list[$lr_i]}_${d_step[$ds_i]}_${g_step[$gs_i]}
+            python ./show_tables.py -dataset $dataset -output ../metrics/$dataset/${model_name}_${mr}_${sample_size}_${sample_id}_${dlr_list[$bs_i]}_${glr_list[$bs_i]}_${d_step[$ds_i]}_${g_step[$gs_i]}
         done 
     done
 done
