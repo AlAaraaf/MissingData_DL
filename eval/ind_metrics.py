fix bugs for metric calculation

eval/ind_metrics.py
@@ -3,35 +3,36 @@ import pandas as pd
 from sklearn import metrics as mtr
 from scipy.spatial.distance import jensenshannon
 
-def get_prob(labels):
+def get_prob(labels, max_label):
     test = pd.Series([i for i in labels])
     counts = []
-    for i in range(int(max(labels))):
+    for i in range(max_label):
         counts.append(sum(x == i for x in labels))
     counts = [x / len(test) for x in counts]
     return counts
 
-def categorical_metrics(pred, target):    
-    acc = mtr.accuracy_score(target, pred)
-    precision, recall, fscore, _ = mtr.precision_recall_fscore_support(target, pred, average='weighted')
+def categorical_metrics(pred, target, compare_loc):    
+    acc = mtr.accuracy_score(target[compare_loc], pred[compare_loc])
+    precision, recall, fscore, _ = mtr.precision_recall_fscore_support(target[compare_loc], pred[compare_loc], average='weighted', zero_division=np.nan)
 
-    p_pred = get_prob(pred)
-    p_target = get_prob(target)
+    max_label = int(max(target))
+    p_pred = get_prob(pred, max_label)
+    p_target = get_prob(target, max_label)
     js_div = jensenshannon(p_pred, p_target)
     return acc, precision, recall, fscore, js_div
 
-def numeric_metrics(pred, target):
+def numeric_metrics(pred, target, compare_loc):
     norm_pred = pred / sum(pred)
     norm_pred = norm_pred.astype(np.float64)
     norm_target = target / sum(target)
     norm_target = norm_target.astype(np.float64)
 
-    mse = mtr.mean_squared_error(target, pred)
-    mae = mtr.mean_absolute_error(target, pred)
+    mse = mtr.mean_squared_error(norm_target[compare_loc], norm_pred[compare_loc])
+    mae = mtr.mean_absolute_error(norm_target[compare_loc], norm_pred[compare_loc])
     js_div = jensenshannon(norm_pred, norm_target)
     return mse, mae, js_div
 
-def get_metrics(complete_data, imputed_data_folder, data_level, impute_num, sample_id =0):
+def get_metrics(complete_data, mask_data, imputed_data_folder, data_level, impute_num, sample_id =0):
     cat_index = data_level['cat_index']
     num_index = data_level['num_index']
     
@@ -48,9 +49,12 @@ def get_metrics(complete_data, imputed_data_folder, data_level, impute_num, samp
         imputed_data = np.loadtxt(current_imputed_dir, delimiter=",").astype(np.float32)
 
         for y_loc in cat_index:
+            compare_loc = np.where(mask_data[:,y_loc])[0]
+            if len(compare_loc) == 0:
+                continue
             target = complete_data[:, y_loc]
             pred = imputed_data[:, y_loc]
-            acc, precision, recall, fscore, js_div = categorical_metrics(pred, target)
+            acc, precision, recall, fscore, js_div = categorical_metrics(pred, target, compare_loc)
             
             js_div_list[y_loc, i] = js_div
             acc_list[y_loc, i] = acc
@@ -59,9 +63,12 @@ def get_metrics(complete_data, imputed_data_folder, data_level, impute_num, samp
             fscore_list[y_loc, i] = fscore
         
         for y_loc in num_index:
+            compare_loc = np.where(mask_data[:,y_loc])[0]
+            if len(compare_loc) == 0:
+                continue
             target = complete_data[:, y_loc]
             pred = imputed_data[:, y_loc]
-            mse, mae, js_div = numeric_metrics(pred, target)
+            mse, mae, js_div = numeric_metrics(pred, target, compare_loc)
             
             mse_list[y_loc, i] = mse
             mae_list[y_loc, i] = mae

eval_main.py
@@ -41,11 +41,17 @@ if __name__ == '__main__':
     print('Calculating metrics......')
 
     complete_data_path = '../training_data/samples/{}/complete_{}_{}/sample_{}.csv'.format(dataset, mr, size, sample_id)
+    sample_data_path = '../training_data/samples/{}/MCAR_{}_{}/sample_{}.csv'.format(dataset, mr, size, sample_id)
+    
     complete_data = np.loadtxt(complete_data_path, delimiter=",").astype(np.float32)
+    sample_data = np.loadtxt(sample_data_path, delimiter=",").astype(np.float32)
+
+    mask_data = np.isnan(sample_data).astype(np.float32)
+    
     imputed_data_folder = '../training_data/results/{}/MCAR_{}_{}/{}/'.format(dataset, mr, size, model)
     data_level = np.load('./datalevel.npy', allow_pickle=True).item()[dataset]
 
-    metrics = get_metrics(complete_data, imputed_data_folder, data_level, impute_num, sample_id)
+    metrics = get_metrics(complete_data, mask_data, imputed_data_folder, data_level, impute_num, sample_id)
     save_path = '../metrics/{}/{}/MCAR_{}_{}/{}/'.format(args.type, dataset, mr, size, model)
     pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
     